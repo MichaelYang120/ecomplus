6966d004990db8b2ccde1b795e4ffcd6dd1b1519
MichaelYang120 <michaelyangiscoding@gmail.com>
2023-03-30 18:03:51 -0500

    added api request, and changes in main.tsx

---
+++ src/Api/Apirequest.tsx
@@ -0,0 +1,5 @@
+export const Productget = () => {
+    fetch('https://fakestoreapi.com/products')
+        .then(res => res.json())
+        .then(json => console.log(json))
+}
--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -1,14 +1,23 @@
 import React, { Component } from 'react'
+import { Productget } from '../Api/Apirequest';
+
+const clickHandler = () => {
+  console.log("working");
+  Productget();
+}
 
 export default class Main extends Component {
+
   render() {
     return (
         <>
-        <div className='mainconatiner'>
-                <div>this is the main content</div>
-
-        </div>
+          <div className='mainconatiner'>
+            <div>this is the main content</div>
+            <button onClick={clickHandler}>Test</button>
+          </div>
         </>
     )
   }
 }
+
+
