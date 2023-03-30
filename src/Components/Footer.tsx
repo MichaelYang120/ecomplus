cd0f9215d87cc1b70327520c9a23292d43a12b6c
MichaelYang120 <michaelyangiscoding@gmail.com>
2023-03-29 22:25:01 -0500

    added components footer/header/main

---
+++ src/Components/Footer.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        <div className='footercontainer'>
+            <div>this is a test</div>
+            <div>this is a test</div>
+            <div>this is a test</div>
+        </div>
+      </footer>
+    )
+  }
+}
---
+++ src/Components/Headers.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Headers extends Component {
+  render() {
+    return (
+      <header>
+        <div className='headercontainer'>
+          <div>LOGO</div>
+          <div>My Ecom Plus</div>
+          <div>Icons</div>
+        </div>
+      </header>
+    )
+  }
+}
