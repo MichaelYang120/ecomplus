5eddf3f8a249954807912bab793397dedd46e436
MichaelYang120 <michaelyangiscoding@gmail.com>
2023-03-31 23:33:47 -0500

    added to app.css and main.tsx

--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -1,56 +1,37 @@
 import React, { useEffect, useState } from 'react'
 import { Productget } from '../Api/Apirequest';
 
-type subject = {
-
-  category : String,
-  description : String,
-  id : Number,
-  image : String,
-  price : Number,
-  rating : { rate: Number, count: Number }
-  title : String
-}
-
 export default function Main() {
 
   const [products, setProducts] = useState([])
 
   useEffect(() => {
-    async function test() {
+    async function getproducts() {
 
       var product = await Productget();
       setProducts(product)
     }
-    test()
+    getproducts()
   }, [])
 
 
     return (
     <>
       <div className='mainconatiner'>
-          {/* <div>{products.map(({ id, title, description, image }) =>
-            <li key={id}>{title}{description}{image}</li>)}
-            </div> */}
-
-            {products.map(({ id, title, description, image, price }) => 
-              <div className="productcontainer" key={id}>
-                <div className='productinnercontainer'>
-                  <label className='productname'>{title}</label>
-                  <p className='productdesc'>{description}</p>
-                  <p className='productprice'>{price}</p>
-                  <div className='buttoncontainer'>
-                    <button className='shopbtn'>Add To Cart</button>
-                    <button className='shopbtn'>Buy Now</button>
-                  </div>
-                </div>
-                <img className='productimage' src={image} alt=""/>
+        {products.map(({ id, title, description, image, price }) => 
+          <div className="productcontainer" key={id}>
+            <div className='productinnercontainer'>
+              <h2 className='productname'>{title}</h2>
+              <p className='productdesc'>{description}</p>
+              <p className='productprice'>${price}</p>
+              <div className='buttoncontainer'>
+                <button className='shopbtn'>Buy Now</button>
+                <button className='shopbtn'>Add To Cart</button>
               </div>
-              
-            
-            )}
-
-        {/* <div>this is the main content test</div> */}
+            </div>
+            <img className='productimage' src={image} alt=""/>
+          </div>
+        )}
       </div>
     </>
   )
