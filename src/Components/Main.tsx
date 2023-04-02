b5708b543afc207393c2026399c84088ecea3416
MichaelYang120 <michaelyangiscoding@gmail.com>
2023-04-02 17:36:26 -0500

    added to Main.tsx

--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -16,10 +16,10 @@ export default function Main() {
   const [productarray, setProductarray] = useState<PArray[]>([])
   const [cartarray, setCartarray] = useState<PArray[]>([])
   const [cartpopup, setCartpopup] = useState(false);
-  
-  
+
+
   // click events
-  const buyhandler = (event:any) => {
+  const buyhandler = (event: any) => {
     var targetname = event.target.title;
     var targetprice = event.target.value;
     var targetimgurl = event.target.getAttribute("data-img");
@@ -33,7 +33,7 @@ export default function Main() {
     setCount(count + 1)
   }
 
-  const addtocarthandler = (event:any) => {
+  const addtocarthandler = (event: any) => {
     var targetname = event.target.title;
     var targetprice = event.target.value;
     var targetimgurl = event.target.getAttribute("data-img");
@@ -41,12 +41,12 @@ export default function Main() {
       console.log("target image" + targetimgurl)
       console.log("event" + event.target)
     }
-    var newproductarray:any = {
-      "name" : targetname,
-      "price" : targetprice,
-      "image" : targetimgurl
+    var newproductarray: any = {
+      "name": targetname,
+      "price": targetprice,
+      "image": targetimgurl
     }
-    if (debug === true ) {
+    if (debug === true) {
 
       console.log(newproductarray)
     }
@@ -61,14 +61,19 @@ export default function Main() {
     setCartarray(resultarray)
   }
 
-  const headercarthandler = (event:any) => {
+  const headercarthandler = (event: any) => {
     if (debug === true) {
       // console.log("cart btn: " + productarray)
       console.log(cartarray)
     }
-    var mycart:any = {"mycart":cartarray};
+    var mycart: any = { "mycart": cartarray };
     console.log(mycart)
     setCartpopup(true);
+  }
+
+  const closepopup = () => {
+    setCartpopup(false);
+
   }
 
   useEffect(() => {
@@ -81,29 +86,32 @@ export default function Main() {
 
   function showcartpopup() {
     return (
-      cartarray.map(({name, price, image}) => 
-        <div className="cartpopupcontainer">
-          <div className='cartpopupname'>{name}</div>
-          <div className='cartpopupprice'>{price}</div>
-          <div className='cartpopupprice'>{image}</div>
+      cartarray.map(({ name, price, image }) =>
+      <div className="cartpopupcontainer">
+          {/* <button onClick={closepopup}>close</button> */}
+          <div className="cartpopupproductcontainer">
+            <div className='cartpopupname'>{name}</div>
+            <div className='cartpopupprice'>{price}</div>
+            <img className='cartpopupimage' src={image} />
+          </div>
         </div>
       )
     )
   }
 
-    return (
+  return (
     <>
       <div className='mainconatiner'>
+        {cartpopup && showcartpopup()}
         <div className="cartcontainer">
-          <div className='cart' onClick={headercarthandler}>{count > 0 ? "Number of Items In My Cart: " + count : "Cart Is Empty"}</div>
-            {cartpopup && showcartpopup()}
-            <div className={'cartpopup'}>
-            </div>
+          <div className='cart' onClick={headercarthandler}>{count > 0 ? "Number of Items In My Cart: " + count : "Cart Is Empty"}
+
+          </div>
           <div className='headercheckout'>
             <a className='headercheckouttext' href='/'>Checkout</a>
           </div>
         </div>
-        {products.map(({ id, title, description, image, price }) => 
+        {products.map(({ id, title, description, image, price }) =>
           <div className="productcontainer" key={id}>
             <div className='productinnercontainer'>
               <h2 className='productname'>{title}</h2>
@@ -116,7 +124,7 @@ export default function Main() {
                 <button className='shopbtn' onClick={addtocarthandler} title={title} value={price} data-img={image}>Add To Cart</button>
               </div>
             </div>
-            <img className='productimage' src={image} alt=""/>
+            <img className='productimage' src={image} alt="" />
           </div>
         )}
       </div>
